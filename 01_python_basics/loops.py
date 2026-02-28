d03b266d0c7d6dfde4ccd1e0f5439170f17060d8
Mohammad Faizan <faizank319957@gmail.com>
2026-02-28 10:46:32 -0500

    add patterns using while loop and for loop

--- 01_python_basics/loops.py
+++ 01_python_basics/loops.py
@@ -9,6 +9,7 @@ This module covers:
 - while loops
 - break / continue
 - backend-style examples
+- pattern using while and for loop
 
 Author: Mohammad Faizan
 DAte : 26/02/2026
@@ -106,7 +107,176 @@ def backend_example():
 
     print("Total Revenue:", total)
     print("-" * 30)
+    
+#---------------------------------------------
+# 7. pattern (using while loop)
+#--------------------------------------------
 
+#---------------------------------------------
+# left aligned  right triangle
+#-----------------------------------------------
+
+def left_aligned_triangle(rows: int = 7) -> None:
+    """
+    Prints a left-aligned right triangle pattern.
+
+    Args:
+        rows (int): Number of rows in the triangle.
+    """
+    i = 1
+
+    while i <= rows:
+        j = 1
+        while j<=rows:
+            if j <= i:
+                print("*", end=" ")
+            else:
+                print(" ", end = " ")
+            j += 1
+
+        print()  # Move to next line
+        i += 1
+        
+#----------------------------------------------
+#  right aligned triangle 
+#----------------------------------------------
+
+print("---right aligned triangle----")
+def right_aligned_triangle(rows: int = 7) -> None:
+    """
+    Prints a right-aligned right triangle pattern.
+
+    Args:
+        rows (int): Number of rows in the triangle.
+    """
+    i = 1
+
+
+    while i <= rows:
+        j = 1
+        while j<=rows:
+            if j >= i:
+                print("*", end=" ")
+            else:
+                print(" ", end = " ")
+            j += 1
+
+        print()  # Move to next line
+        i += 1
+        
+#--------------------------------------------------
+#   pyramid pattern
+#---------------------------------------------------
+
+def pyramid(rows: int=7)->None:
+    """
+    print a star centered pyramid pattern
+
+    Args:
+        rows (int): Number of the rows in the pyramid
+    """
+    i = 1
+    total_columns = 2 * rows - 1   # dynamic column calculation
+
+    while i <= rows:
+        j = 1
+        while j <= total_columns:
+            if j >= rows - (i - 1) and j <= rows + (i - 1):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+            j += 1
+        print()
+        i += 1
+        
+#---------------------------------------------------
+#    N-pattern using while loop
+#---------------------------------------------------
+
+def n_star(rows: int = 7)-> None:
+    """
+    prints a N star pattern.
+    
+    args:
+        rows (int): Number of rows in the triangle
+    """
+    i = 1
+    while i <= rows:
+        j = 1
+        while j <= rows:
+            if j == 1 or j == i or j == rows:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+            j += 1
+        print()
+        i += 1
+        
+#=====================================================
+#      pattern using for loop
+#======================================================
+
+#-----------------------------------------------------
+#   Z pattern using for loop
+#-----------------------------------------------------
+
+def z_pattern(rows: int=9)-> None:
+    """
+    prints a Z-star pattern 
+    args:
+        rows(int): Number of rows in the triangle
+    """
+    for i in range(1,rows+1):
+        for j in range(1,rows+1):
+            if i==1 or i== rows or j == rows + 1 - i:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+        print()
+        
+#--------------------------------------------------------
+# Number left aligned triangle
+#-------------------------------------------------------
+def number_left_aligned_triangle(rows: int = 9) -> None:
+    """
+    Prints a left-aligned number triangle where
+    each row contains the row number repeated.
+
+    Example (rows = 5):
+
+    1
+    2 2
+    3 3 3
+    4 4 4 4
+    5 5 5 5 5
+    """
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(i, end=" ")
+        print()
+
+        
+#--------------------------------------------------------
+#  Continous number left aligned triangle
+#-------------------------------------------------------
+def continuous_number_triangle(rows: int = 5) -> None:
+    """
+    Prints a left-aligned triangle with continuous numbers.
+
+    Example (rows = 4):
+
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    """
+    num = 1
+
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(num, end=" ")
+            num += 1
+        print()
 
 # ----------------------------------------
 # 7. Main Execution Block
@@ -118,3 +288,17 @@ if __name__ == "__main__":
     continue_example()
     nested_loop_example()
     backend_example()
+    print("---left aligned triangle----")
+    left_aligned_triangle()
+    print("---right aligned triangle----")
+    right_aligned_triangle()
+    print("----Centered pyramid star pattern")
+    pyramid()
+    print("--- N-pattern using star---")
+    n_star()
+    print("----Z pattern Using for loop----")
+    z_pattern()
+    print("---number left aligned triangle---")
+    number_left_aligned_triangle()
+    print("---continuous number triangle---")
+    continuous_number_triangle()
